136071bbfeb898966ff70ab360d7564a593e4392
msingh9 <manish.rurki@gmail.com>
2019-02-24 18:24:42 -0800

    Add train/dev split script

---
+++ scripts/gen_train_dev_data.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import nltk
+import random
+import numpy as np
+import re
+
+dataDir = "../data"
+textbooks = ("open_stax_anatomy_physiology",
+             "open_stax_astronomy",
+             "open_stax_biology_2e",
+             "open_stax_chemistry_2e",
+             "open_stax_microbiology",
+             "open_stax_university_physics_v3")
+
+sentences = []
+tags = []
+
+for book in textbooks:
+    print ("Reading book %s" %(book))
+    sfname = dataDir + "/textbooks_extracted/" + book + "_sentences.txt"
+    tfname = dataDir + "/textbooks_extracted/" + book + "_sentence_tags.txt"
+    with open(sfname, "r") as fin:
+        data = fin.read()
+        for sent in data.split("\n"):
+            if re.search("^$", sent):
+                continue
+            tokens = nltk.word_tokenize(sent)
+            sentences.append(" ".join(tokens))
+
+    with open(tfname, "r") as fin:
+        data = fin.read()
+        for sent in data.split("\n"):
+            if re.search("^$", sent):
+                continue
+            tags.append(sent)
+    break
+
+print (len(sentences))
+print (len(tags))
+
+assert(len(sentences) == len(tags))
+
+## sanity check
+for sent, tag in zip(sentences, tags):
+    print (len(sent), len(tag))
+    assert(len(sent) == len(tag))
+            
+## re-shuffle and split 80-20%
+indices = np.arange(len(sentences))
+np.random.shuffle(indices)
+indices = list(indices)
+
+dev_size = 0.2
+length = len(sentences)
+train_size = int(length*dev_size)
+debug_size = 10
+
+with open(dataDir + "/full/train/sentences.txt", "w") as fout:
+    fout.write("\n".join([sentences[x] for x in indices[0:train_size]]))
+with open(dataDir + "/full/train/labels.txt", "w") as fout:
+    fout.write("\n".join([tags[x] for x in indices[0:train_size]]))
+
+with open(dataDir + "/full/dev/sentences.txt", "w") as fout:
+    fout.write("\n".join([sentences[x] for x in indices[train_size:]]))
+with open(dataDir + "/full/dev/labels.txt", "w") as fout:
+    fout.write("\n".join([tags[x] for x in indices[train_size:]]))
+
+# small
+with open(dataDir + "/small/train/sentences.txt", "w") as fout:
+    fout.write("\n".join([sentences[x] for x in indices[0:debug_size]]))
+with open(dataDir + "/small/train/labels.txt", "w") as fout:
+    fout.write("\n".join([tags[x] for x in indices[0:debug_size]]))
+
+with open(dataDir + "/small/dev/sentences.txt", "w") as fout:
+    fout.write("\n".join([sentences[x] for x in indices[debug_size:debug_size*2]]))
+with open(dataDir + "/small/dev/labels.txt", "w") as fout:
+    fout.write("\n".join([tags[x] for x in indices[debug_size:debug_size*2]]))
+
+
+
+
